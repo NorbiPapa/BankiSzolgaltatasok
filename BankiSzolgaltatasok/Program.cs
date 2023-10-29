23d93f331a963fbeb7ab929c1134c48e4eb5c372
Norbert Varga <norbika0210@gmail.com>
2023-10-29 17:16:24 +0100

    asd

--- BankiSzolgaltatasok/Program.cs
+++ BankiSzolgaltatasok/Program.cs
@@ -4,9 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            Tulajdonos tulajdonos1 = new Tulajdonos("Nagy Tomi");
-            Tulajdonos tulajdonos2 = new Tulajdonos("Kis Tomi");
-            Tulajdonos tulajdonos3 = new Tulajdonos("Közepes Tomi");
+            Tulajdonos tulajdonos1 = new Tulajdonos("Tök Ödön");
+            Tulajdonos tulajdonos2 = new Tulajdonos("Káromló Krisztián");
+            Tulajdonos tulajdonos3 = new Tulajdonos("Ku Kíra");
 
             Bank bank = new Bank();
 
@@ -22,22 +22,22 @@
 
 
             Számla szamla1 = bank.szamlanyitas(0, tulajdonos1);
-            szamla1.Befizet(10000);
+            szamla1.Befizet(1301);
 
             Számla szamla2 = bank.szamlanyitas(10000, tulajdonos2);
-            szamla2.Befizet(5000);
+            szamla2.Befizet(5050);
 
             Számla szamla3 = bank.szamlanyitas(2000, tulajdonos3);
-            szamla3.Befizet(6000);
-            szamla3.Befizet(4000);
+            szamla3.Befizet(9000);
+            szamla3.Befizet(5000);
 
             Számla szamla4 = bank.szamlanyitas(3000, tulajdonos3);
-            szamla4.Befizet(4000);
+            szamla4.Befizet(4500);
 
 
 
             Console.WriteLine("Új kártya igénylése");
-            Kártya kartya1 = szamla1.ÚjKártya("5555 5555 5555 5555");
+            Kártya kartya1 = szamla1.ÚjKártya("6666 7777 8888 9999");
             Console.WriteLine($"Kártyaszám: {kartya1.KartyaSzam} Tulajdonos: {kartya1.Tulajdonos.Nev}");
             Console.WriteLine("Vásárlás");
             Console.WriteLine($"Egyenleg vásárlás előtt: {szamla1.AktualisEgyenleg} ft");
